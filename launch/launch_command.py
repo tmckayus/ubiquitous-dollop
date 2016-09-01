b07704e87fa2db42f3794d2974fd7095fa78c6b5
Trevor McKay <tmckay@redhat.com>
2016-09-01 10:37:38 -0400

    Set output to /tmp

--- launch/launch_command.py
+++ launch/launch_command.py
@@ -51,8 +51,8 @@ def main(*args):
         # Interpret all command line args as the command to run
         a = subprocess.Popen(cargs,
                              env=env,
-                             stdout=open("stdout", "w"),
-                             stderr=open("stderr", "w"))
+                             stdout=open("/tmp/stdout", "w"),
+                             stderr=open("/tmp/stderr", "w"))
 
         # Set our handler to trap SIGINT and propagate to the child
         # The expectation is that the child process handles SIGINT
@@ -61,13 +61,13 @@ def main(*args):
 
         # Write out the childpid just in case there is a
         # need to send special signals directly to the child process
-        open("childpid", "w").write("%s\n" % a.pid)
+        open("/tmp/childpid", "w").write("%s\n" % a.pid)
 
         # Wait for child to exit and write result file
         log.info("Waiting for subprocess %s" % a.pid)
         ret = a.wait()
         log.info("Subprocess exit status %s" % ret)
-        open("result", "w").write("%s\n" % ret)
+        open("/tmp/result", "w").write("%s\n" % ret)
 
     except Exception as e:
         log.exception(e)
